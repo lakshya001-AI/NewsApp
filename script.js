e359ba496f7a98c9983eb5eb24a96f97161aa399
Lakshya Makode <makodelakshya101@gmail.com>
2024-02-11 00:26:35 +0530

    commit changes

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 
+const api = "https://newsapi.org/v2/top-headlines?country=us&apiKey=184d4a3d26584262af13d0eef4e3c610";
+async function getNews(){
+    let response = await fetch(api);
+    let data = await response.json();
+    console.log(data.articles);
+}
+
+
+getNews();
 
