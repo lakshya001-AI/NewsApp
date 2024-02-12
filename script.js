6e5dd04bbbed63b329921ba7b3baa458c8fb096a
Lakshya Makode <makodelakshya101@gmail.com>
2024-02-12 16:09:06 +0530

    commit cnahges

--- script.js
+++ script.js
@@ -3,14 +3,14 @@ const api = "https://newsapi.org/v2/top-headlines?country=us&apiKey=184d4a3d2658
 async function getNews(){
     let response = await fetch(api);
     let data = await response.json();
-    console.log(data.articles[3]);
+    console.log(data.articles[10]);
 
-    document.getElementById("newsimg").src = data.articles[3].urlToImage;
-    document.getElementById("title").innerText = data.articles[3].title;
-    document.getElementById("author").innerText = data.articles[3].author;
-    document.getElementById("publisher").innerText = data.articles[3].source.name;
-    document.getElementById("description").innerText = data.articles[3].description;
-    document.getElementById("readmore").href = data.articles[3].url;
+    document.getElementById("newsimg").src = data.articles[10].urlToImage;
+    document.getElementById("title").innerText = data.articles[10].title;
+    document.getElementById("author").innerText = data.articles[10].author;
+    document.getElementById("publisher").innerText = data.articles[10].source.name;
+    document.getElementById("description").innerText = data.articles[10].description;
+    document.getElementById("readmore").href = data.articles[10].url;
 
 }
 
